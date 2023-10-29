f4d958160cd1d1d1754eb2d97c0e12ae91cb6978
Bobby Pinard <bobby.pinard@gmail.com>
2023-10-29 12:24:54 -0500

    Got both Redis and web containers working

--- web/main.py
+++ web/main.py
@@ -1,23 +1,9 @@
-import time
-
-import redis
-from flask import Flask
-
-app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
-
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
+import flask
+app = flask.Flask(__name__)
 
 @app.route('/')
-def hello():
-    count = get_hit_count()
-    return 'Testing this Hello World! I have been seen {} times.\n'.format(count)
+def hello_world():
+    return 'Flask Dockerized'
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
